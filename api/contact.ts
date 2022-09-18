273d31d3f4284149fbf3de5c6eb6da1489745f55
Julian <85513960+JLN13X@users.noreply.github.com>
2022-09-18 17:58:37 +0200

    prepare contact form

---
+++ api/contact.ts
@@ -0,0 +1,11 @@
+import type { VercelApiHandler } from "@vercel/node";
+
+const handler: VercelApiHandler = (request, response) => {
+  response.status(200).json({
+    body: request.body,
+    query: request.query,
+    cookies: request.cookies,
+  });
+};
+
+export default handler;
