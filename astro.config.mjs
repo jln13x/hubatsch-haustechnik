199ee28e6b53d956a66800d6b0dbe1d2f11f52f0
Julian <85513960+JLN13X@users.noreply.github.com>
2022-09-20 00:04:05 +0200

    add prod config

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,13 +5,20 @@ import sitemap from "@astrojs/sitemap";
 
 const site = "https://staging-hubatsch-haustechnik.hubatsch.dev";
 
+const integrations = [
+  tailwind(),
+  sitemap({
+    filter: (page) => !page.includes("datenschutz"),
+  }),
+];
+
+console.log(process.env.PUBLIC_VERCEL_ENV);
+
+if (process.env.PUBLIC_VERCEL_ENV === "production") {
+  integrations.push(compress());
+}
+
 export default defineConfig({
   site,
-  integrations: [
-    tailwind(),
-    sitemap({
-      filter: (page) => !page.includes("datenschutz"),
-    }),
-    compress(),
-  ],
+  integrations: integrations,
 });
