5ad25380dd4c39695ccd891ffffec880d8107cae
Julian <85513960+JLN13X@users.noreply.github.com>
2022-09-20 00:17:15 +0200

    remove env logs

--- astro.config.mjs
+++ astro.config.mjs
@@ -12,15 +12,8 @@ const integrations = [
   }),
 ];
 
-console.log(process.env.PUBLIC_VERCEL_ENV);
-console.log({
-  env: process.env,
-});
-
-console.log(process.env.VERCEL_ENV);
-
-if (process.env.PUBLIC_VERCEL_ENV === "production") {
-  integrations.push(compress());
+if (process.env.VERCEL_ENV === "production") {
+  integrations.push(compress({ logger: 2 }));
 }
 
 export default defineConfig({
