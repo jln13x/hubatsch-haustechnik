2bbdf48ee42d761bb2295e5d462f36f8dd5e30df
Julian <85513960+JLN13X@users.noreply.github.com>
2022-10-01 20:20:00 +0200

    image optimization

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import compress from "astro-compress";
 import sitemap from "@astrojs/sitemap";
+import { astroImageTools } from "astro-imagetools";
 
 const site = "https://staging-hubatsch-haustechnik.hubatsch.dev";
 
@@ -10,6 +11,7 @@ const integrations = [
   sitemap({
     filter: (page) => !page.includes("datenschutz"),
   }),
+  astroImageTools,
 ];
 
 if (process.env.VERCEL_ENV === "production") {
